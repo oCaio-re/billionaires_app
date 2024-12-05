063abc5a0097a7c65c2236f208b59ac20f2b1a79
Caio Oliveira <xcaio2@gmail.com>
2024-12-05 17:21:43 +0000

    almost done

--- views.py
+++ views.py
@@ -8,13 +8,13 @@ import db
 
 @app.route('/')
 def home():  # put application's code here
-    conn = sqlite3.connect('DB/billionaires')
+    conn = sqlite3.connect('DB/Billionaires.db')
     cursor = conn.cursor()
     top10 = conn.execute(
         '''
-        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.THE_SOURCE
+        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.SOURCE
         FROM BILLIONAIRES b JOIN COUNTRIES c
-        ON b.CITIZENSHIP = c.NAME
+        ON b.ID_CITIZENSHIP = c.ID
         LIMIT 10
         ; 
         ''').fetchall()
@@ -25,13 +25,13 @@ def home():  # put application's code here
 
 @app.route('/top10')
 def top10():
-    conn = sqlite3.connect('DB/billionaires')
+    conn = sqlite3.connect('DB/Billionaires.db')
     cursor = conn.cursor()
     top10 = conn.execute(
         '''
-        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.THE_SOURCE
+        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.SOURCE
         FROM BILLIONAIRES b JOIN COUNTRIES c
-        ON b.CITIZENSHIP = c.NAME
+        ON b.ID_CITIZENSHIP = c.ID
         LIMIT 10
         ; 
         ''').fetchall()
@@ -42,34 +42,76 @@ def top10():
 
 @app.route('/top10/country/<country>') #get top ty BY COUNTRY
 def get_top10_by_country(country):
-    conn = sqlite3.connect('DB/billionaires')
+    conn = sqlite3.connect('DB/Billionaires.db')
     # using prepared statement to avoid sql injection: https://en.wikipedia.org/wiki/SQL_injection#Root_causes
     top10 = conn.execute(
-        f"SELECT POSITION, FULL_NAME, WEALTH, CITIZENSHIP, THE_SOURCE "
-        f"FROM BILLIONAIRES "
-        f"WHERE CITIZENSHIP = ? COLLATE NOCASE "
-        f"ORDER BY WEALTH DESC "
-        f"LIMIT 10;"
+        '''
+        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.SOURCE 
+        FROM BILLIONAIRES b JOIN COUNTRIES c
+        ON b.ID_CITIZENSHIP = c.ID 
+        WHERE c.NAME = ? COLLATE NOCASE 
+        ORDER BY WEALTH DESC 
+        LIMIT 10;
+        '''
     , (country, )).fetchall()
     conn.commit()
     conn.close()
     return render_template('top10/top10-queries.html', top10=top10, country=country)
 
 
-# @app.route('/top10/country/<industry>') #get top ty BY COUNTRY
-# def get_top10_by_industry(industry):
-#     conn = sqlite3.connect('DB/billionaires')
-#     # using prepared statement to avoid sql injection: https://en.wikipedia.org/wiki/SQL_injection#Root_causes
-#     top10 = conn.execute(
-#         f"SELECT POSITION, FULL_NAME, WEALTH, CITIZENSHIP, THE_SOURCE "
-#         f"FROM BILLIONAIRES "
-#         f"WHERE CITIZENSHIP = ? COLLATE NOCASE "
-#         f"ORDER BY WEALTH DESC "
-#         f"LIMIT 10;"
-#     , (country, )).fetchall()
-#     conn.commit()
-#     conn.close()
-#     return render_template('top10/top10-queries.html', top10=top10, country=country)
+@app.route('/all-list') #get top ty BY COUNTRY
+def get_all_list():
+    conn = sqlite3.connect('DB/Billionaires.db')
+    # using prepared statement to avoid sql injection: https://en.wikipedia.org/wiki/SQL_injection#Root_causes
+    all_rank = conn.execute(
+        '''
+        SELECT b.POSITION, b.FULL_NAME, b.WEALTH, c.NAME, b.SOURCE 
+        FROM BILLIONAIRES b JOIN COUNTRIES c
+        ON b.ID_CITIZENSHIP = c.ID
+        ;
+        '''
+
+    , ).fetchall()
+    conn.commit()
+    conn.close()
+    return render_template('all_list/all_list.html', all_rank=all_rank)
+
+
+@app.route('/countries')
+def get_countries():
+    conn = sqlite3.connect('DB/Billionaires.db')
+    cursor = conn.cursor()
+    countries = conn.execute(
+        '''
+        SELECT NAME, CONTINENT, TAX_RATE, POPULATION, LIFE_EXPECTANCY
+        FROM COUNTRIES
+        ;
+        ''').fetchall()
+    conn.commit()
+    conn.close()
+    return render_template('countries/countries.html', countries=countries)
+
+@app.route('/subject/<subject>')
+def get_subject(subject):
+    conn = sqlite3.connect('DB/Billionaires.db')
+    current = conn.execute(
+        '''
+        SELECT b.POSITION, b.FULL_NAME, b.GENDER, b.WEALTH, c.NAME, b.SOURCE, i.INDUSTRY, b.ID
+        FROM BILLIONAIRES b JOIN COUNTRIES c JOIN INDUSTRIES i
+        ON b.ID_CITIZENSHIP = c.ID AND b.ID_INDUSTRY = i.ID
+        WHERE b.FULL_NAME LIKE ? 
+        ;
+        ''', (subject, )).fetchone()
+    next_and_prev = conn.execute(
+        '''
+        SELECT FULL_NAME
+        FROM BILLIONAIRES 
+        WHERE ID IN (?, ?) 
+        ;
+        ''', (current[7] - 1, current[7] + 1)).fetchall()
+    conn.commit()
+    conn.close()
+    return render_template('subject/subject.html', current=current, previous=next_and_prev[0], next=next_and_prev[1])
 
 @app.route('/all_list')
 def all_list():
